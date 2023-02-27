d0f77c7aca44472f929fa385ac3df077da0c648d
ronitshinde <rony.shinde753@gmail.com>
2023-02-27 20:01:08 +0530

    [Ronit] Add : UC1-Insertion Sort

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertionSort insertion = new InsertionSort();
+            Console.WriteLine("Select The Option");
+            Console.WriteLine("1.Insertion Sort");
+            Console.WriteLine();
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    insertion.DisplayInsertionSort();
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
